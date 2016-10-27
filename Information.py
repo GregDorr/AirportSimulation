64e61d1622af1bdfc45b1202d47261bc0d1481bd
Greg Dorr <dorrg@augsburg.edu>
2016-10-27 15:38:21 -0500

    updated Information

--- Information.py
+++ Information.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
+
+# This class holds the information coming in from each plane
 class Information:
-    Id = None
-    subTime = None
-    slotReq = None
-    length = None
-    actualStartTime = 0
-    actualEndTime = 0
+
+    Id = None               # The plane's id
+    subTime = None          # the sub mission time
+    slotReq = None          # the slot they request
+    length = None           # the length of time they request
+    actualStartTime = 0     # the time when they actually started
+    actualEndTime = 0       # the time they actually take off
 
     # constructor that accepts an id, submission time
     # time slot requested and the length
@@ -20,9 +23,4 @@ class Information:
     def __str__(self):
         tup = str(self.Id), ",", str(self.subTime), ",", str(self.slotReq), ",", str(self.length)
         return ''.join(tup)
-
-    # display the start and end time with the id
-    def display_results(self):
-        output = self.Id + " (" + str(self.actualStartTime) + "-", str(self.actualEndTime) + ")"
-        return ''.join(output)
 
