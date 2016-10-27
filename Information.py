43eef90df36d23f94180cac9499632c50307cd60
Greg Dorr <dorrg@augsburg.edu>
2016-10-27 10:21:36 -0500

    updated Information

--- Information.py
+++ Information.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 
-
 class Information:
     Id = None
     subTime = None
     slotReq = None
     length = None
-    actualStartTime = None
-    actualEndTime = None
+    actualStartTime = 0
+    actualEndTime = 0
 
     # constructor that accepts an id, submission time
     # time slot requested and the length
@@ -19,41 +18,11 @@ class Information:
 
     # method that displays the string version of each object
     def __str__(self):
-        tup = "Id: ", self.Id, ", subTime: ", self.subTime, ", Slot: ", self.slotReq, ", Duration: ", self.length
+        tup = str(self.Id), ",", str(self.subTime), ",", str(self.slotReq), ",", str(self.length)
         return ''.join(tup)
 
-    # method to set the actual start time
-    def set_time_start(self, start):
-        self.actualStartTime = start
+    # display the start and end time with the id
+    def display_results(self):
+        output = self.Id + " (" + str(self.actualStartTime) + "-", str(self.actualEndTime) + ")"
+        return ''.join(output)
 
-    # method to set the actual end time
-    def set_time_end(self, end):
-        self.actualEndTime = end
-
-    # returns the object
-    def get_information(self):
-        return self
-
-    # returns the id
-    def get_id(self):
-        return self.Id
-
-    # returns the submission time
-    def get_subTime(self):
-        return self.subTime
-
-    # returns slot requested
-    def get_slotReq(self):
-        return self.slotReq
-
-    # returns the length
-    def get_length(self):
-        return self.length
-
-    # returns start time
-    def get_start_time(self):
-        return self.actualStartTime
-
-    # returns end time
-    def get_end_time(self):
-        return self.actualEndTime
